f4567c37b423c830642a1c4bdc49a8c7e5e46d0a
Francisco Marín Pérez <fmarin-p@student.42madrid.com>
2022-04-23 17:33:42 +0200

    125%

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,25 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmarin-p <fmarin-p@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: fmarin-p <fmarin-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/06 17:24:27 by fmarin-p          #+#    #+#             */
-/*   Updated: 2022/04/21 12:33:30 by fmarin-p         ###   ########.fr       */
+/*   Created: 2022/04/20 17:15:48 by fmarin-p          #+#    #+#             */
+/*   Updated: 2022/04/23 17:30:21 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_list
+{
+	int				fd;
+	char			*memory;
+	struct s_list	*next;
+
+}	t_list;
+
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
-void	*ft_calloc(size_t count, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_concat_str(char *buf, char *memory);
 size_t	ft_strlen(const char *str);
+int		free_list(t_list **head, t_list *node, t_list *tmp);
+int		storage_memory(int fd, t_list **head, char *memory);
 
 #endif
